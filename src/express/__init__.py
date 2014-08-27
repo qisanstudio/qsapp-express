18743f93b6c093ba123a7b908c69a37f53b65954
qisanstudio <qisanstudio@gmail.com>
2014-08-27 16:33:47 +0800

    add accout and role

--- src/express/__init__.py
+++ src/express/__init__.py
@@ -8,11 +8,12 @@ from studio.core.engines import db
 
 app = StudioFlask(__name__)
 
-app.secret_key = '\x94\xabM\x8c\xc8\r_x#\x06\x8ac\x99\xf5/\x83\xe7\xce\x04\x80XVs\xbe'
 db.init_app(app)
 Babel(app=app, default_locale='zh')
 
 
 with app.app_context():
-    from express import views
-    
+    from microsite.panel import admin
+    app.add_url_rule('/apps/%s/<path:filename>' %
+                        app.name, endpoint='static', #subdomain='static',
+                        view_func=app.send_static_file)
