ed6f8fedff6e915ce36776f2e5cd7767fbb8a144
qisanstudio <qisanstudio@gmail.com>
2014-08-30 15:38:35 +0800

    add blueprints

--- src/express/__init__.py
+++ src/express/__init__.py
@@ -15,4 +15,14 @@ Babel(app=app, default_locale='zh')
 
 with app.app_context():
     from express import views
-    
+    from microsite.panel import admin
+    from microsite.blueprints import blueprint_www
+    admin.init_app(app)
+
+    assert views
+
+    app.register_blueprint(blueprint_www)
+
+    app.add_url_rule('/apps/%s/<path:filename>' %
+                        app.name, endpoint='static', #subdomain='static',
+                        view_func=app.send_static_file)
--- src/express/blueprints.py
+++ src/express/blueprints.py
@@ -1,2 +1,4 @@
 # -*- code: utf-8 -*-
 from __future__ import unicode_literals
+
+blueprint_www = Blueprint('views', __name__)
